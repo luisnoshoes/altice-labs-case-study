58bf5af63f3eb591398b736e20c3e4af999e1eeb
luisnoshoes <luis.f.descalco@gmail.com>
2024-08-16 17:48:56 +0100

    Added material, navbar and first component

---
+++ src/app/condition-form/condition-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-condition-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './condition-form.component.html',
+  styleUrl: './condition-form.component.scss'
+})
+export class ConditionFormComponent {
+
+}
