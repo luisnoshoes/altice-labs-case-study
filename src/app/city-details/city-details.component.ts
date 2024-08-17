0a18927930fa2aefe82d31d8f721068e038255aa
luisnoshoes <luis.f.descalco@gmail.com>
2024-08-17 19:09:50 +0100

    City details table

--- src/app/city-details/city-details.component.ts
+++ src/app/city-details/city-details.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Condition } from '../models/condition';
 
 @Component({
   selector: 'app-city-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule],
   templateUrl: './city-details.component.html',
   styleUrl: './city-details.component.scss'
 })
-export class CityDetailsComponent {
+export class CityDetailsComponent implements AfterViewInit, OnChanges{
+  @Input({required: true}) conditions!: Condition[];
+  dataSource = new MatTableDataSource<Condition>();
+  readonly columns = ['date', 'temperature', 'altitude', 'rainingStatus', 'networkPower'];
 
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator!;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['conditions']){
+      this.dataSource.data = changes['conditions'].currentValue;
+      if(this.paginator){
+        this.dataSource.paginator = this.paginator!;
+      }
+    }
+  }
 }
--- src/app/condition-overview/condition-overview.component.ts
+++ src/app/condition-overview/condition-overview.component.ts
@@ -7,6 +7,8 @@ import { DateTime } from 'luxon';
 import { CityOverviewListComponent } from '../city-overview-list/city-overview-list.component';
 import { CityDetailsComponent } from '../city-details/city-details.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ActivatedRoute } from '@angular/router';
+import { catchError, pipe, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-condition-overview',
@@ -17,16 +19,29 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 })
 export class ConditionOverviewComponent {
   conditions: Condition[] | undefined;
-  cityOverviewList: CityOverview[] | undefined;
 
-  constructor(private readonly conditionsService: ConditionsService, private readonly snackBar: MatSnackBar) { }
+  cityOverviewList: CityOverview[] | undefined;
+  selectedCityConditions: Condition[] | undefined;
+
+  constructor(private readonly conditionsService: ConditionsService, private readonly snackBar: MatSnackBar, private readonly route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.conditionsService.getConditions().subscribe({
-      next: (conditions) => {
+    this.conditionsService.getConditions().pipe(
+      tap((conditions) => {
         this.conditions = conditions;
         this.cityOverviewList = this.getCityOverviewList(conditions);
-      },
+      }),
+      switchMap(() => this.route.queryParamMap),
+      tap((paramMap) => {
+        const selectedCity = paramMap.get('city');
+
+        if (selectedCity) {
+          this.selectedCityConditions = this.conditions!
+            .filter((condition) => condition.city === selectedCity)
+            .sort((a, b) => DateTime.fromISO(b.date).toMillis() -  DateTime.fromISO(a.date).toMillis());
+        }
+      })
+    ).subscribe({
       error: () => this.snackBar.open("Failed retrieve conditions", 'Close')
     })
   }
@@ -39,10 +54,13 @@ export class ConditionOverviewComponent {
     for (const city of cities) {
       const cityConditions = conditions.filter((condition) => condition.city === city)
 
+      const lastUpdate = DateTime.max(...cityConditions.map((condition) => DateTime.fromISO(condition.date)))
+      const lastNetworkPower = cityConditions.find((condition) => condition.date === lastUpdate.toISODate())!.networkPower;
+
       const cityOverview: CityOverview = {
         name: city,
-        lastUpdate: DateTime.max(...cityConditions.map((condition) => DateTime.fromISO(condition.date))),
-        lastNetworkPower: Math.max(...cityConditions.map((condition) => condition.networkPower)),
+        lastUpdate: lastUpdate,
+        lastNetworkPower: lastNetworkPower,
         avgTemperature: cityConditions.map((condition) => condition.temperature).reduce((a, b) => a + b) / cityConditions.length,
         avgRainingStatus: cityConditions.map((condition) => condition.rainingStatus).reduce((a, b) => a + b) / cityConditions.length,
         avgAltitude: cityConditions.map((condition) => condition.altitude).reduce((a, b) => a + b) / cityConditions.length,
