f641f43fab2300a5ad75b776b99c0f4b12946a67
luisnoshoes <luis.f.descalco@gmail.com>
2024-08-18 18:56:26 +0100

    Nav translations and active language highlight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, RouterModule],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, RouterModule, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'altice-labs-case-study';
+  language = 'en';
 
   constructor(private readonly translateService: TranslateService){}
 
   onClickLanguage(language: string): void {
+    this.language = language
     this.translateService.use(language);
   }
 }
